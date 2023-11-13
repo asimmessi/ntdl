393ac8c63966668004ed7aedc512c135ce027669
Asim Shrestha <asimmessi1@gmail.com>
2023-11-13 12:22:33 +1100

    second commit

--- client/src/helper/axiosHelper.js
+++ client/src/helper/axiosHelper.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const apiEP = "http://localhost:8000/api/v1/task";
+const apiEP = process.env.NODE_ENV !=='production'
+?"/api/v1/task":
+"http://localhost:8000/api/v1/task" ;
 
 export const postTask = async (obj) => {
   try {
@@ -8,6 +10,10 @@ export const postTask = async (obj) => {
     return data;
   } catch (error) {
     console.log(error);
+    return {
+      status: "error",
+      message: error.message,
+    }
   }
 };
 
